e8f7fae58b3d3c42e39919789ce14f337928d0af
Tushar Kumar <145555246+tusharkumar9@users.noreply.github.com>
2026-08-13 17:14:26 +0530

    Time: 2 ms (70.92%), Space: 55.5 MB (60.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,25 +1,53 @@
+// Solution by the HASHMAP METHOD
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (map.containsKey(nums[i])) {
+//                 map.put(nums[i], map.get(nums[i]) + 1);
+//             }
+//             else {
+//                 map.put(nums[i], 1);
+//             }
+//         }
+
+//         int majority = (nums.length / 2);
+//         int result = 0;
+
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if (entry.getValue() > majority) {
+//                 result = entry.getKey();
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
+
+//Solution by the Boyer Voting ALgo
+
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+
+        int candidate = 0;
+        int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }
-            else {
-                map.put(nums[i], 1);
-            }
-        }
 
-        int majority = (nums.length / 2);
-        int result = 0;
+            if (count == 0) {
+                candidate = nums[i];
+            }
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > majority) {
-                result = entry.getKey();
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
 
-        return result;
+        return candidate;
     }
 }
