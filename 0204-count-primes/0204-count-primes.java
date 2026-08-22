c470d90faa7ea340911e8de9917254fffd03eea6
Tushar Kumar <145555246+tusharkumar9@users.noreply.github.com>
2026-08-22 23:15:26 +0530

    Time: 791 ms (5%), Space: 79.9 MB (5.36%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,48 @@
+// class Solution {
+//     // brute force
+//     public static boolean prime(int a) {
+//         if (a <= 1) return false; 
+        
+//         for (int i = 2; i < a; i++) {
+//             if (a % i == 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public int countPrimes(int n) {
+//         int totalPrimes = 0;
+//         for (int i = 2; i < n; i++) {
+//             if (prime(i)) {
+//                 totalPrimes++; 
+//             }
+//         }
+        
+//         return totalPrimes;
+//     }
+// }
+class Solution {  
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        //Sieve of Eratosthenes
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
