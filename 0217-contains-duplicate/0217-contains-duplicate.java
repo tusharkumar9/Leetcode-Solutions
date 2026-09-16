549432cf531db51b0693edd2b09888a334c67ea2
Tushar Kumar <145555246+tusharkumar9@users.noreply.github.com>
2026-09-16 23:32:34 +0530

    Time: 18 ms (76.87%), Space: 108 MB (56.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -18,19 +18,35 @@
 //     }
 // }
 
-import java.util.HashSet;
+// import java.util.HashSet;
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for (int num : nums) {
+//             if (!set.add(num)) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
-            if (!set.add(num)) {
+      Set<Integer> intSet= new HashSet<>();
+        
+        for(int i =0; i<nums.length; i++){
+            if(intSet.contains(nums[i])) {
                 return true;
+            } else {
+                intSet.add(nums[i]);
             }
         }
-
         return false;
+        
     }
 }
